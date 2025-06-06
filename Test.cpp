ffb306e51428af9802ea24d761d8f5980c0bc7b1
M1roll <gludara104@gmail.com>
2025-06-07 08:12:03 +1000

    Update Test.cpp

--- Test.cpp
+++ Test.cpp
@@ -7,14 +7,31 @@ void Test(){
 	StandVector sv1(4, 7);
 	StandVector sv2(1, 10);
 
+	StandVector sv3(5, 9);
+	StandVector sv4(8, 4);
+
+	StandVector sv5(10, 12);
+
 	StandVector sum_res = _sum(sv1, sv2);
 	assert(sum_res.x == 5 && sum_res.y == 17);
+
+	StandVector sum_res2 = _sum(sv3, sv4);
+	assert(sum_res2.x == 13 && sum_res2.y == 13);
 
 	StandVector razn_res = _razn(sv1, sv2);
 	assert(razn_res.x == 3 && sum_res.y == -3);
 
+	StandVector razn_res2 = _razn(sv3, sv4);
+	assert(razn_res2.x == -3 && sum_res2.y == 5);
+
 	StandVector pr_num_res = _proizv_na_chislo(sv1, 2);
 	assert(pr_num_res.x == 8 && pr_num_res.y == 14);
+
+	StandVector pr_num_res2 = _proizv_na_chislo(sv3, 5);
+	assert(pr_num_res2.x == 25 && pr_num_res2.y == 45);
+
+	StandVector pr_num_res3 = _proizv_na_chislo(sv5, 3);
+	assert(pr_num_res.x == 30 && pr_num_res.y == 36);
 
 	assert(_long_vector(sv1) == 3 && _long_vector(sv2) == 9);
 
